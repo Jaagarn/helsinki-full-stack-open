fea8bf35e300feb43819559f9e60f66758c71543
Jesper Skoglund <jesper.skoglund@solita.fi>
2023-03-27 10:11:05 +0200

    part 3.3 done

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,6 +1,6 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
 let persons = [
   {
@@ -25,22 +25,34 @@ let persons = [
   },
 ];
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
 
-app.get('/info', (request, response) => {
+app.get("/info", (request, response) => {
   response.send(
-    `<p>Phonebook has info for  ${persons.length} people</p>`+
-    `<p>${new Date().toString()}</p>`
-    )
-})
+    `<p>Phonebook has info for ${persons.length} people</p>` +
+      `<p>${new Date().toString()}</p>`
+  );
+});
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
 
-const PORT = 3001
+  if (person) {
+    response.json(person);
+  } else {
+    response.statusMessage = "Person not found";
+    response.status(404).end();
+  }
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
