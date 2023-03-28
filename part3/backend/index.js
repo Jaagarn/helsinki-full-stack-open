24c64fa6229191609e98291ad25a9f6ada9366ce
Jesper Skoglund <jesper.skoglund@solita.fi>
2023-03-28 16:29:13 +0200

    deployed to fly.io

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 var morgan = require("morgan");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 morgan.token("body", (req, res) => JSON.stringify(req.body));
 app.use(
@@ -111,7 +113,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
