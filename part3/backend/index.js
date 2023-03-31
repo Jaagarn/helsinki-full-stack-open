12393b86724bd4d2e13f1a0b9afdc27a662fa124
Jesper Skoglund <jesper.skoglund@solita.fi>
2023-03-31 17:26:50 +0200

    up to 3.18

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -15,43 +15,18 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-const generateId = () => {
-  // Math.floor is maybe not a great solution, but it should work here. It's random enough
-  return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
-};
-
 app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
+  response.send("<!DOCTYPE html>" + "<h1>Hello World!</h1>");
 });
 
 app.get("/info", (request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${persons.length} people</p>` +
-      `<p>${new Date().toString()}</p>`
-  );
+  Person.find({}).then((personsResponse) => {
+    response.send(
+      "<!DOCTYPE html>" +
+        `<p>Phonebook has info for ${personsResponse.length} people</p>` +
+        `<p>${new Date().toString()}</p>`
+    );
+  });
 });
 
 app.get("/api/persons", (request, response) => {
@@ -89,13 +64,28 @@ app.post("/api/persons", (request, response) => {
   });
 });
 
-app.delete('/api/persons/:id', (request, response, next) => {
-  Person.findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end()
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
     })
-    .catch(error => next(error))
-})
+    .catch((error) => next(error));
+});
+
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
