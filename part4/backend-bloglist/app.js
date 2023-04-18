26d6633d72d621194086cad201e46a9fc993afcc
Jaagarn <skoglundjesper@hotmail.com>
2023-04-18 10:53:23 +0200

    4.20 dpme

--- part4/backend-bloglist/app.js
+++ part4/backend-bloglist/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
-const loginRouter = require('./controllers/login')
+const loginRouter = require("./controllers/login");
 
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
@@ -16,10 +16,11 @@ const setUpExpressApp = () => {
   app.use(express.json());
   app.use(express.static("build"));
   app.use(middleware.requestLogger);
+  app.use(middleware.tokenExtractor);
 
   app.use("/api/blogs", blogsRouter);
   app.use("/api/users", usersRouter);
-  app.use('/api/login', loginRouter)
+  app.use("/api/login", loginRouter);
 
   app.use(middleware.unknownEndpoint);
   app.use(middleware.errorHandler);
--- part4/backend-bloglist/utils/middleware.js
+++ part4/backend-bloglist/utils/middleware.js
@@ -2,6 +2,7 @@ const logger = require("./logger");
 var morgan = require("morgan");
 
 morgan.token("body", (request, response) => JSON.stringify(request.body));
+
 const morganInstance = morgan(
   ":method :url :status :res[content-length] - :response-time ms :body"
 );
@@ -23,15 +24,26 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: "malformatted id" });
   } else if (error.name === "ValidationError") {
     return response.status(400).json({ error: error.message });
-  } else if (error.name ===  'JsonWebTokenError') {
-    return response.status(400).json({ error: error.message })
-  } 
-  
+  } else if (error.name === "JsonWebTokenError") {
+    return response.status(400).json({ error: error.message });
+  }
+
   next(error);
+};
+
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get("authorization");
+
+  if (authorization && authorization.startsWith("Bearer ")) {
+    request.token = authorization.replace("Bearer ", "");
+  }
+
+  next();
 };
 
 module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
+  tokenExtractor,
 };
