688971004ef356b90e79ac0b198cffad7b752ac9
Jaagarn <skoglundjesper@hotmail.com>
2023-04-18 10:22:34 +0200

    4.18 done

--- part4/backend-bloglist/app.js
+++ part4/backend-bloglist/app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
+
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require('./controllers/login')
+
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
@@ -16,6 +19,7 @@ const setUpExpressApp = () => {
 
   app.use("/api/blogs", blogsRouter);
   app.use("/api/users", usersRouter);
+  app.use('/api/login', loginRouter)
 
   app.use(middleware.unknownEndpoint);
   app.use(middleware.errorHandler);
