800a5f5491d530eba84a2e46afd2544b72285a10
Jaagarn <skoglundjesper@hotmail.com>
2023-07-10 16:39:37 +0200

    6.21 done

--- part6/query-anecdotes/src/components/AnecdoteForm.js
+++ part6/query-anecdotes/src/components/AnecdoteForm.js
@@ -1,21 +1,31 @@
+import { useMutation, useQueryClient } from "react-query";
+import { postOneAnecdote } from "../services/anecdote";
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient();
+  const newAnecdoteMutation = useMutation(postOneAnecdote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("anecdotes");
+    },
+  });
 
   const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+    event.preventDefault();
+    const content = event.target.anecdote.value;
+    event.target.anecdote.value = "";
+    newAnecdoteMutation.mutate({ content });
+    console.log("new anecdote");
+  };
 
   return (
     <div>
       <h3>create new</h3>
       <form onSubmit={onCreate}>
-        <input name='anecdote' />
+        <input name="anecdote" />
         <button type="submit">create</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default AnecdoteForm
+export default AnecdoteForm;
--- part6/query-anecdotes/src/services/anecdote.js
+++ part6/query-anecdotes/src/services/anecdote.js
@@ -1,4 +1,14 @@
-import axios from 'axios'
+import axios from "axios";
 
-export const getAnecdotes = () =>
-  axios.get('http://localhost:3001/anecdotes').then(res => res.data)
+const baseUrl = "http://localhost:3001/anecdotes";
+
+export const getAnecdotes = async () => {
+  const respone = await axios.get(baseUrl);
+  return respone.data;
+};
+
+export const postOneAnecdote = async (anecdoteContent) => {
+  const anecdote = { content: anecdoteContent.content, votes: 0 };
+  const response = await axios.post(baseUrl, anecdote);
+  return response.data;
+};
